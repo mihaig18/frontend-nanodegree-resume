42f595b883ce4c295457b35b668c5a91b274cc3d
Mihai Grigore <mihaig18@gmail.com>
2016-09-25 19:53:49 +0300

    Test replace function

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,4 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
- $("#main").append("Grigore Mihai");
+var email = "mihaig18@yahoo.com"
+var newEmail = email.replace("yahoo", "gmail");
+console.log(email);
+console.log(newEmail);
