ce91d8e86ced5014e84763209b56a85e52ac1b6f
Mihai Grigore <mihaig18@gmail.com>
2016-09-25 20:10:14 +0300

    Add funThoughts to page

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,3 +2,8 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
  $("#main").append("Grigore Mihai");
+
+ var funThoughts = "I am Mihai and I am awesome"
+ funThoughts.replace("awesome", "fun");
+ 
+ $("#main").append(funThoughts);
